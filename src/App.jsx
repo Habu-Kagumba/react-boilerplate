caa2ec6dbe5d90e9f97bd67418293e054dbad444
Herbert Kagumba <habukagumba@gmail.com>
2019-01-19 16:51:38 +0300

    Minimal boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import mainStyles from "./assets/styles/styles.scss";
+
+const App = () => (
+  <section className={mainStyles.main}>
+    <div className={mainStyles.header}>
+      <span className={mainStyles.rivetOne} />
+      <span className={mainStyles.title}>React Boilerplate</span>
+      <span className={mainStyles.rivetTwo} />
+    </div>
+  </section>
+);
+
+export default App;
